739eab51e708edcc6c823c84840ada4378fa1946
jamesD33064 <james0120160379@gmail.com>
2023-08-19 23:27:58 +0800

    新增Component LibraryCard

---
+++ components/LibraryCard/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { Component, useState } from "react";
+
+interface LibraryCardProps {
+  id: string;
+  storyName: string;
+  storyImg: string;
+  showTag: boolean;
+  link: string;
+}
+
+export default function LibraryCard(props: LibraryCardProps) {
+  return (
+    <>
+      <div id={props.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="w-full h-auto aspect-square overflow-hidden">
+          <img
+            src={props.storyImg}
+            alt=""
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="px-6">
+          <div className="font-bold py-2">{props.storyName}</div>
+        </div>
+        {props.showTag && (
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #小紅帽
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #大野狼
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #紅色帽T
+            </span>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -6,15 +6,15 @@ const nextConfig = {
     appDir: true,
   },
 
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api-proxy/:path*', // 匹配代理請求的路徑
-        destination: 'http://140.134.37.23:8000/:path*', // 阿芬學校伺服器路徑
-      },
-    ];
-  },
+  // reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api-proxy/:path*', // 匹配代理請求的路徑
+  //       destination: 'http://140.134.37.23:8000/:path*', // 阿芬學校伺服器路徑
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
